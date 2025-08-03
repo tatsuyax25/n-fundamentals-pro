3ef572a1d99b458072de6b03b2967bc57504e1e7
Miguel Urena <urena_miguel82@outlook.com>
2025-08-03 14:35:48 -0700

    CRUD in controller

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Put, Delete, Post } from '@nestjs/common';
 
 @Controller('songs')
 export class SongsController {
+  @Post()
+  create() {
+    return 'Create a new song endpoint';
+  }
   @Get()
   findAll() {
     return 'Find all songs';
+  }
+  @Get(':id')
+  findOne() {
+    return 'Fetch song on the based on ID';
+  }
+
+  @Put(':id')
+  update() {
+    return 'Update song on the based on ID';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'Delete song on the based on ID';
   }
 }
