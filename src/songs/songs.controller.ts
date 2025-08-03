68601e2b8cb6c995ab74e9047803d511ae6ec5c8
Miguel Urena <urena_miguel82@outlook.com>
2025-08-03 16:39:09 -0700

    Need to fix bugs

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, Put, Delete, Post } from '@nestjs/common';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
+  constructor(private songsService: SongsService) {}
   @Post()
   create() {
-    return 'Create a new song endpoint';
+    return this.songsService.create('Animals by Martin Garrix');
   }
   @Get()
   findAll() {
-    return 'Find all songs';
+    return this.songsService.findAll();
   }
   @Get(':id')
   findOne() {
